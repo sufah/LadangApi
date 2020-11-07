2bff9368b3c0bf71f26a1c96e6153289cb6e0b91
SU FAH LIAU <liau@sufah.net>
2020-11-08 06:03:26 +0800

    created PlantationsController.cs

---
+++ LadangApi/Controllers/PlantationsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using LadangApi.Interface;
+using LadangApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LadangApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantationsController : ControllerBase
+    {
+        private IPlantationDbContext _plantationDbContext;
+
+        public PlantationsController(IPlantationDbContext plantationDbContext)
+        {
+            _plantationDbContext = plantationDbContext;
+        }
+
+
+        // GET: api/<PlantationsController>
+        [HttpGet]
+        public IEnumerable<Plantation> Get()
+        {
+            return _plantationDbContext.Plantations;
+        }
+
+        // GET api/<PlantationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlantationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlantationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlantationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
