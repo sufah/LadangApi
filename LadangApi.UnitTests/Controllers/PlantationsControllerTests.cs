84ca1433f480f310bcf665fc419bab6dbfc72cb9
SU FAH LIAU <liau@sufah.net>
2020-11-08 06:13:38 +0800

    created PlantationsControllerTests.cs and removed UnitTest1.cs

---
+++ LadangApi.UnitTests/Controllers/PlantationsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LadangApi.UnitTests.Controllers
+{
+    [TestFixture]
+    public class PlantationsControllerTests
+    {
+        [Test]
+        public void Get_WhenCalled_ReturnAllPlantationDetails()
+        {
+            // Arrange
+            
+            
+            // Act
+            
+            // Assert
+        }
+    }
+}
