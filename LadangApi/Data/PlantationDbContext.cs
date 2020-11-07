5c5833fad029993255314558d1d774d1c2f57417
SU FAH LIAU <liau@sufah.net>
2020-11-08 05:44:02 +0800

    created PlantationDbContext.cs

---
+++ LadangApi/Data/PlantationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LadangApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LadangApi.Data
+{
+    public class PlantationDbContext : DbContext
+    {
+        public PlantationDbContext(DbContextOptions<PlantationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Plantation> Plantations { get; set; }
+    }
+}
